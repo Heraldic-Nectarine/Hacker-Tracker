ec36ea6f46fdd79b33af0f0e758395f7b641626d
son-david <sondavid92@gmail.com>
2015-11-12 19:02:46 -0800

    added basic io.socket removed grunt tasks

--- server/server.js
+++ server/server.js
@@ -1,12 +1,20 @@
 var express = require('express');
-var mongoose = require('mongoose');
-
 var app = express();
+
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
 
 var port = process.env.PORT || 8000;
 
 require('./config/middleware.js')(app, express);
 
-app.listen(port);
+server.listen(port);
+
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
 
 module.exports = app;
