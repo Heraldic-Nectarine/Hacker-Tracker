257f554f5443c8b606e6d8b6a67eb9c392ba881e
Matt Sumnicht <masumnicht@gmail.com>
2015-11-12 19:13:32 -0800

    Code cleanup;

--- client/app/map/map.js
+++ client/app/map/map.js
@@ -2,21 +2,21 @@ angular.module('app.map', [])
 
 .controller('MapController', ['$scope', function ($scope) {
   // methods to be used inside map.html
-  $scope.latitude = ''
-  $scope.longitude = ''
-  $scope.locationCheck = function() {
+  $scope.latitude = '';
+  $scope.longitude = '';
+  $scope.locationCheck = function () {
     if (navigator.geolocation) {
       console.log('Geolocation is supported!');
     } else {
       console.log('Geolocation is not supported for this Browser/OS version yet.');
     }
     var startPos;
-    var geoSuccess = function(position) {
+    var geoSuccess = function (position) {
       startPos = position;
       
       $scope.latitude = startPos.coords.latitude;
       $scope.longitude = startPos.coords.longitude;
-      
+
     };
     navigator.geolocation.getCurrentPosition(geoSuccess);
   }
