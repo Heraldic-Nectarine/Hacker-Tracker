d5ace740ac0c2b5083e9bb06403234695a05e408
William <william93lee@gmail.com>
2015-11-13 16:22:40 -0800

    Fixed profile pic on fb

--- client/app/facebook/facebook.js
+++ client/app/facebook/facebook.js
@@ -5,33 +5,36 @@ angular.module('app.facebook', ['ngOpenFB'])
   $scope.me = {};
   $openFB.init( {appId: '909462752470016'})
 
-  $openFB.login({scope: 'email, user_friends'});
-
-  $openFB.api({path: '/me'})
+  $openFB.login({scope: 'email, user_friends'})
   .then(function (res) {
-    angular.extend($scope.me, res);
-  }, function( err ) {
-    console.log(err);
-  });
-  
-  $openFB.api({path: '/me/friends'})
-  .then(function (res) {
-    angular.extend($scope.me, res);
-  }, function (err) {
-    console.log(err);
+
+    $openFB.api({path: '/me'})
+    .then(function (res) {
+      angular.extend($scope.me, res);
+    }, function( err ) {
+      console.log(err);
+    });
+
+    $openFB.api({path: '/me/friends'})
+    .then(function (res) {
+      angular.extend($scope.me, res);
+    }, function (err) {
+      console.log(err);
+    });
+
+    $openFB.api({
+      path: '/me/picture',
+      params: {
+          redirect: false,
+          height: 64,
+          width: 64
+      }
+    }).then(function( res ) {
+      angular.extend($scope.me, {picture: res.data.url});
+      ServerInteraction.getFBdata($scope.me);
+    });
   });
 
-  $openFB.api({
-    path: '/me/picture',
-    params: {
-        redirect: false,
-        height: 64,
-        width: 64
-    }
-  }).then(function( res ) {
-    angular.extend($scope.me, {picture: res.data.url});
-    ServerInteraction.getFBdata($scope.me);
-  });
 
 
 
