511680bb45cff0c714ddc73ea4019c8de9533dcd
son-david <sondavid92@gmail.com>
2015-11-16 16:45:29 -0800

    added map name to map

--- client/app/map/map.js
+++ client/app/map/map.js
@@ -9,7 +9,7 @@ angular.module('app.map', ['ngOpenFB'])
   $scope.user.latitude = '';
   $scope.user.longitude = '';
 
-  $scope.mapName = ClientHelper.storage2[0];
+  $scope.mapName = "";
 
   $scope.tempDataStore;
   $scope.intervalFunc;
@@ -47,7 +47,8 @@ angular.module('app.map', ['ngOpenFB'])
     }
   }
 
-  $scope.startInterval = function (){
+  $scope.init = function (){
+    $scope.mapName = ClientHelper.storage2[0];
     socket.emit('init', ClientHelper.storage2[0]);
     $scope.intervalFunc = $interval($scope.locationCheck, 3000);
   }
