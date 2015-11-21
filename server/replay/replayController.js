205db3b7cb44e8b617b59afa87b3d29c0d162aa8
Will Dembinski <willdembinski@gmail.com>
2015-11-21 11:28:27 -0800

    A little bit of cleanup

--- server/replay/replayController.js
+++ server/replay/replayController.js
@@ -3,7 +3,7 @@ var Replay = require('./replayModel.js');
 module.exports = {
 
 	getAllReplays:function ( req,resp ) {
-		console.log("GET REQUEST --- GetAllReplays --- ")
+		console.log("GET REQUEST --- GetAllReplays --- ");
 		var options = {};
 		var limit = req.params.limit || 20; // <--default num of response.  Change if youre complaining atm.
 		if ( req.params.owner ) options.owner = req.params.owner;
@@ -15,11 +15,7 @@ module.exports = {
 			options.created = { $gt : afterDate };
 		}
 
-		Replay.find(
-
-			options
-
-		).limit( limit ).exec(function ( err,response ) {
+		Replay.find(options).limit(limit).exec(function ( err,response ) {
 			if(err) {
 				console.log("=-=-=-=-=-=-=-=-=GET-ALL-REPLAYS=-=-=-=-=-=-=-=-= \n",err);
 				return resp.send("Something went wrong. GETALLREPLAYS",404);
@@ -33,12 +29,7 @@ module.exports = {
 		options = {};
 		if(req.params.title) options.title = req.params.title;
 		if(req.params.owner) options.owner = req.params.owner;
-
-		Replay.findOne(
-
-			options
-
-		).exec(function(err,response){
+		Replay.findOne(options).exec(function(err,response){
 			if(err){
 				console.log("=-=-=-=-=-=-=-=-=GET-ONE-REPLAY=-=-=-=-=-=-=-=-= \n",err);
 				return resp.send("Something went wrong. GETONEREPLAY",404);
@@ -49,6 +40,7 @@ module.exports = {
 	},
 
 	insertReplay:function (req, resp) { //assumes payload resembles `{owner:'someone',title:'myStupidTitle',"path":"[{"lat":"123.33","lng":"321.44"},{"lat":"123.33","lng":"321.44"},{"lat":"123.33","lng":"321.44"}]"}`
+
 		var newReplay = new Replay(req.body);
 		newReplay.save(function(err,replayObj){
 
@@ -56,8 +48,7 @@ module.exports = {
 				console.log("=-=-=-=-=-=-=-=-=INSERT-REPLAY=-=-=-=-=-=-=-=-= \n",err);
 				return res.send("Something went wrong. INSERTREPLAY",404);
 			}
-			console.log("Success")
-			// resp.send("Success");
+			console.log("Success");
 			resp.send(replayObj);
 		});
 
