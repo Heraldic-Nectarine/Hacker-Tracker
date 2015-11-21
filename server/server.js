305c915766c164059906eddcf615f912e8124eb3
Chris Bee <chris@ctrlbee.com>
2015-11-21 12:09:49 -0800

    Updating map to handle multiple rooms

--- server/server.js
+++ server/server.js
@@ -14,26 +14,47 @@ app.use(ExRouter);
 router(ExRouter);
 server.listen(port);
 
-var storage = {};
+var currentUsersInRoom = [];
+var currentRoom = "";
 
 io.on('connection', function (socket) {
-  socket.on('init', function (data) {
-    socket.join('/'+data);
-    //set socket.room before adding data
+  socket.on('connectToRoom', function (room) {
+    console.log("room on server", room);
+    socket.join(room);
 
-    //storage === room 
-    storage[data] = {};
-    socket.on('userData', function (info) {
-      //info === $scope.user from the front end
-      storage[data][info.id] = info;
-      socket.emit('serverData', storage[data]);
+    socket.on('userData', function (user) {
+      currentUsersInRoom.push(user);
+      console.log(currentUsersInRoom);
+      socket.emit('serverData', currentUsersInRoom);
     });
-    socket.on('logout', function (info) {
-      delete storage[data][info];
-      socket.leave('/'+data);
-      socket.emit('serverData', storage[data]);
+
+    socket.on('logout', function (user) {
+      delete currentUsersInRoom[user];
+      socket.leave(room);
+      socket.emit('serverData', currentUsersInRoom);
     })
   });
 });
+
+
+// io.on('connection', function (socket) {
+//   socket.on('init', function (data) {
+//     socket.join('/'+data);
+//     //set socket.room before adding data
+
+//     //storage === room 
+//     storage[data] = {};
+//     socket.on('userData', function (info) {
+//       //info === $scope.user from the front end
+//       storage[data][info.id] = info;
+//       socket.emit('serverData', storage[data]);
+//     });
+//     socket.on('logout', function (info) {
+//       delete storage[data][info];
+//       socket.leave('/'+data);
+//       socket.emit('serverData', storage[data]);
+//     })
+//   });
+// });
 
 module.exports = app;
