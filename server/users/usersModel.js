21f2042f99212d04cb541ee45ce8545dd0f963cb
GregDungca <greg.dungca@gmail.com>
2015-11-29 18:11:59 -0800

    Completed first iteration of user model and made small changes to room model

---
+++ server/users/usersModel.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose');
+
+var UserRoomSchema = new mongoose.Schema({
+  name: String
+}, { _id: false});
+
+var AdminRoomSchema = new mongoose.Schema({
+  name: String
+}, { _id: false});
+
+var UserSchema = new mongoose.Schema({
+  userName: String,
+  email: String,
+  password: String,
+  profilePic : String
+});
+
+module.exports = mongoose.model('User', UserSchema);
